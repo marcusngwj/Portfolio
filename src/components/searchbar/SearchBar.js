60868fcc643fe7b413c7b20bb614f3408db47c87
marcusngwj <marcusngwj@msn.com>
2018-12-12 02:06:10 +0800

    Implement searchbar

---
+++ src/components/searchbar/SearchBar.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './style.scss';
+import TextField, {HelperText, Input} from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+
+class SearchBar extends Component {
+  state = {value: 'Search'};
+
+  render() {
+    return (
+      <div className="searchbar">
+      	<TextField
+          className="container"
+      	  label='school'
+          helperText={<HelperText>Eg. Skills / Project Titles </HelperText>}
+          fullWidth={true}
+          leadingIcon={<MaterialIcon
+                        icon='search'
+                        className="icon-search" />}
+          trailingIcon={<MaterialIcon
+                         icon='cancel'
+                         className="button-cancel"
+                         onClick={() => this.setState({value: 'yay'})} />}
+        >
+          <Input className="input-area"
+            value={this.state.value}
+            onChange={(e) => this.setState({value: e.target.value})}
+          />
+        </TextField>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/scenes/portfolio/PortfolioScene.js
+++ src/scenes/portfolio/PortfolioScene.js
@@ -2,14 +2,15 @@ import React, {Component} from 'react';
 import './style.scss';
 
 import PortfolioMetadata from './PortfolioMetadata.js';
-
+import {Cell, Grid, Row} from '@material/react-layout-grid';
+import SearchBar from 'components/searchbar/SearchBar';
 import CardTheme from 'components/card/CardTheme';
 
 class PortfolioScene extends Component {
   render() {
     return (
       <div className="portfolio-scene">
-      	<CardTheme />
+      	<SearchBar />
       </div>
     );
   }
