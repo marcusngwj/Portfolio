46d614002a6159409be000adaf5ca0b6cae43f47
marcusngwj <marcusngwj@msn.com>
2018-12-14 16:13:27 +0800

    update project banner

--- src/scenes/portfolio/projects/p10005/P10005.js
+++ src/scenes/portfolio/projects/p10005/P10005.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './style.scss';
 
 import banner from './img/banner.png';
+import {TopAppBarFixedAdjust} from '@material/react-top-app-bar';
 
 class P10005 extends Component {
   render() {
     return (
       <div className="p10005">
-      	<img src={banner} className="banner"/>
-      	<h1>P10005</h1>
+      	<TopAppBarFixedAdjust>
+	      <img src={banner} className="banner"/>
+	      
+      	</TopAppBarFixedAdjust>
       </div>
     );
   }
