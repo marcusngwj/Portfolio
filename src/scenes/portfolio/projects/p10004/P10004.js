ee6f64a96f392ba7c7a3e92c3dce44c9747b9089
marcusngwj <marcusngwj@msn.com>
2018-12-26 14:05:24 +0800

    add responsibilities

--- src/scenes/portfolio/projects/p10004/P10004.js
+++ src/scenes/portfolio/projects/p10004/P10004.js
@@ -20,6 +20,38 @@ class P10004 extends Component {
 	        <ButtonIconGithub className="button" outlined label='Github' url='https://github.com/cs3103group3/cs3103' />
 	      </div>
 	    </TopAppBarFixedAdjust>
+	    <Grid>
+	      <Row>
+	        <Cell columns={2}></Cell>
+	      	<Cell columns={8}>
+		      <p className="content-title">Summary</p>
+		      <p className="content-text">
+		        A P2P network consisting of a centralised directory tracker and several peers.
+		        Peers, behind symmetric NAT, are able to inform the tracker on new files available for sharing, as well as download desired chunks of files from other peers.
+		        The tracker maintains a database of these chunk entries and pass information to the peers upon request.
+		        A TURN server is also incorporated into the tracker. 
+		        In this system, only text files are allowed for transfer.
+		        <br /><br />
+		        This project was developed for the module CS3103, Computer Neworks Practice, offered in NUS.
+		      </p>
+		    </Cell>
+		    <Cell columns={2}></Cell>
+		  </Row>
+		  <Row className="content-grid-row">
+	      	<Cell columns={2}></Cell>
+	      	<Cell columns={8}>
+		      <p className="content-title">My Responsibilities</p>
+		      <div className="content-text">
+		        In a team of four, I:
+		        <ul id="responsibility-list">
+		          <li>Suggested the initial overall architecture</li>
+		          <li>Created Heartbat protocol for Tracker and Peers</li>
+		        </ul>
+		      </div>
+		    </Cell>
+		    <Cell columns={2}></Cell>
+		  </Row>
+		</Grid>
 	  </div>
     );
   }
